c8966306ee626371b7e07a0236cf5c7ef98b4576
gunmo <ggm1207@gmail.com>
2020-05-24 22:19:51 +0900

    programmers level2, level3 commit

--- 1845.py
+++ 1845.py
@@ -1,13 +1,14 @@
 def solution(nums):
     N = len(nums) // 2
-    answer = min(N, len(set(nums))) 
+    answer = min(N, len(set(nums)))
     return answer
+
 
 if __name__ == "__main__":
     t_cases = []
-    t_cases.append([[3,1,2,3]])
-    t_cases.append([[3,3,3,2,2,4]])
-    t_cases.append([[3,3,3,2,2,2]])
-    
+    t_cases.append([[3, 1, 2, 3]])
+    t_cases.append([[3, 3, 3, 2, 2, 4]])
+    t_cases.append([[3, 3, 3, 2, 2, 2]])
+
     for tc in t_cases:
         print(solution(*tc))
